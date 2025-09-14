2d54972fee50edc9c9567402dab1944828871053
serg <saaverdo@gmail.com>
2025-09-14 22:59:10 +0000

    Backend bucket added

---
+++ terraform/core/tf_backend/backend_bucket.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "backend" {
+  bucket = "pex-bsv-tf-state-bucket"
+  tags = {
+    Name    = "TF state bucket"
+    Project = "PEX"
+    Task = "CM-J-3"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_backend" {
+  bucket = aws_s3_bucket.backend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+output "state_bucket" {
+  value = aws_s3_bucket.backend.bucket
+}
